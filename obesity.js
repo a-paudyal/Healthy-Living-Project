670028e383a09afe98f64dd63032287a531ee4c5
Aadi Paudyal <71290587+a-paudyal@users.noreply.github.com>
2022-05-23 20:34:24 -0500

    Add files via upload

---
+++ obesity.js
@@ -0,0 +1,9 @@
+var hidden = "";
+
+
+
+function link()
+{
+  var x =  "<a href='https://www.webmd.com/diet/obesity/ss/slideshow-obesity-weight-loss-tips'>Tips to Lose 100 Pounds or More</a>" + "<br><br>" + "<a href='https://www.hsph.harvard.edu/obesity-prevention-source/obesity-prevention/#:~:text=Choosing%20healthier%20foods%20whole%20grains,%2C%20and%20other%20%E2%80%9Csit%20time%E2%80%9D'>Obesity Prevention Strategies</a>" + "<br><br>" + "<a href='https://www.cdc.gov/obesity/strategies/index.html'>Strategies to Prevent and Manage Obesity</a>";
+  document.getElementById("output1").innerHTML = x;
+}
